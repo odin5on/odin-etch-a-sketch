c19cb289023edec884e90a67b10ce0b411355c99
odin5on <108775964+odin5on@users.noreply.github.com>
2023-03-20 13:15:26 -0500

    Implement drawing with a clicked mouse

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ function generateCanvas(){
     row.classList.add('row')
     for(let j = 0; j<16; j++){
       const pixel = document.createElement('div');
+      // const mouseoverEvent = new Event('mouseover');
+      // pixel.dispatchEvent(mouseoverEvent);
       pixel.classList.add('pixel');
+      pixel.addEventListener('mouseover', (e) => {
+        if(e.buttons == 1){
+          e.target.classList.add('black');
+        }
+      });
       row.appendChild(pixel);
     }
     canvas.appendChild(row);
   }
 }
 
-
-
+function drawPixel(){
+  this.classList.add('black');
+}
